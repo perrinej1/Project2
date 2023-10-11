2d11f4e0ecb3dff43a6f855e78870f782c5c17c6
perrinej1 <perrinej1@xavier.edu>
2023-10-11 19:28:21 -0400

    updating with ticks variable

--- kernel/proc.h
+++ kernel/proc.h
@@ -91,7 +91,8 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
-  int tickets;                 // Number of tickets for process lottery scheduler
+  int tickets;                 // Number of tickets a processor has
+  int ticks;                   // Number of tickets each process has accumulated
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
